7088cb36c21887afe92df101faf2495c308a7e2b
sabbir1054 <mdsabbir1054@gmail.com>
2024-08-26 18:04:59 +0600

    about page head complete

--- src/app/[locale]/(withCommonLayout)/about/page.jsx
+++ src/app/[locale]/(withCommonLayout)/about/page.jsx
@@ -1,7 +1,10 @@
 "use client";
 import LanguageSelector from "@/components/Hero/LanguageSelector";
 import { motion } from "framer-motion";
+import { useLocale, useTranslations } from "next-intl";
 const AboutPage = () => {
+  const locale = useLocale();
+  const t = useTranslations("about");
   return (
     <div className="min-h-screen">
       <div
@@ -15,10 +18,25 @@ const AboutPage = () => {
           backgroundPosition: "center center",
         }}
       >
-        <h2 className="text-5xl text-center text-slate-100 font-semibold">
-          About us
+        <h2
+          className={`text-5xl text-center text-slate-100 font-semibold ${
+            locale === "bn" && "bn-font"
+          }`}
+        >
+          {t("head.title")}
         </h2>
-        <p className="text-slate-500 text-center text-xl">Explore Our Galaxy</p>
+        <div className="pt-2 flex justify-center">
+          <div className="w-11/12 md:w-3/4 lg:w-2/4 ">
+            <p
+              className={`text-slate-300 text-center text-xl ${
+                locale === "bn" && "bn-font"
+              }`}
+            >
+              {t("head.description")}
+            </p>
+          </div>
+        </div>
+
         <motion.div
           className="absolute top-4 right-4 z-20"
           initial={{ opacity: 0, x: 50 }} // Start off-screen to the right
