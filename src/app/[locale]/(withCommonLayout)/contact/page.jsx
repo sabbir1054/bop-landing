55755427e18ef95dce7aaa116249e1f6dd96a53d
sabbir1054 <mdsabbir1054@gmail.com>
2024-08-27 09:33:00 +0600

    map updated

--- src/app/[locale]/(withCommonLayout)/contact/page.jsx
+++ src/app/[locale]/(withCommonLayout)/contact/page.jsx
@@ -1,54 +1,77 @@
-"use client"
-import React from 'react';
-import { useLocale, useTranslations } from 'next-intl';
+"use client";
+import LanguageSelector from "@/components/Hero/LanguageSelector";
 import { motion } from "framer-motion";
-import LanguageSelector from '@/components/Hero/LanguageSelector';
+import { useLocale, useTranslations } from "next-intl";
 const ContactPage = () => {
-    const locale = useLocale();
-    const t = useTranslations("contact");
-    return (
-      <div className="min-h-screen ">
-        <div
-          className=" flex justify-center flex-col"
-          style={{
-            height: "40vh",
-            backgroundColor: "black",
-            backgroundImage: `url("/assets/rsz_about-banner.png")`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            backgroundPosition: "center center",
-          }}
+  const locale = useLocale();
+  const t = useTranslations("contact");
+  return (
+    <div className="min-h-screen ">
+      <div
+        className=" flex justify-center flex-col"
+        style={{
+          height: "40vh",
+          backgroundColor: "black",
+          backgroundImage: `url("/assets/rsz_about-banner.png")`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center center",
+        }}
+      >
+        <h2
+          className={`text-5xl text-center text-slate-100 font-semibold ${
+            locale === "bn" && "bn-font"
+          }`}
         >
-          <h2
-            className={`text-5xl text-center text-slate-100 font-semibold ${
-              locale === "bn" && "bn-font"
-            }`}
-          >
-            {t("head.title")}
-          </h2>
-          <div className="pt-2 flex justify-center">
-            <div className="w-11/12 md:w-3/4 lg:w-2/4 ">
-              <p
-                className={`text-slate-300 text-center text-xl ${
-                  locale === "bn" && "bn-font"
-                }`}
-              >
-                {t("head.description")}
-              </p>
-            </div>
+          {t("head.title")}
+        </h2>
+        <div className="pt-2 flex justify-center">
+          <div className="w-11/12 md:w-3/4 lg:w-2/4 ">
+            <p
+              className={`text-slate-300 text-center text-xl ${
+                locale === "bn" && "bn-font"
+              }`}
+            >
+              {t("head.description")}
+            </p>
           </div>
-
-          <motion.div
-            className="absolute top-4 right-4 z-20"
-            initial={{ opacity: 0, x: 50 }} // Start off-screen to the right
-            animate={{ opacity: 1, x: 0 }} // Slide in and fade in
-            transition={{ duration: 0.5, ease: "easeOut" }} // Smooth transition
-          >
-            <LanguageSelector />
-          </motion.div>
         </div>
+
+        <motion.div
+          className="absolute top-4 right-4 z-20"
+          initial={{ opacity: 0, x: 50 }} // Start off-screen to the right
+          animate={{ opacity: 1, x: 0 }} // Slide in and fade in
+          transition={{ duration: 0.5, ease: "easeOut" }} // Smooth transition
+        >
+          <LanguageSelector />
+        </motion.div>
       </div>
-    );
+      <div className="grid grid-cols-1 md:grid-cols-2  ">
+        <div className="flex flex-col justify-center bg-gray-100">
+          <section className=" px-4 about-sec2">
+            <div className="container mx-auto max-w-4xl"></div>
+          </section>
+        </div>
+
+        <motion.div
+          initial={{ opacity: 0, x: 50 }} // Start off-screen to the right
+          animate={{ opacity: 1, x: 0 }} // Slide in and fade in
+          transition={{ duration: 0.5, ease: "easeOut" }} // Smooth transition
+        >
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.983005313934!2d90.39755038601828!3d23.712300923766865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b90035d679ef%3A0x8abb3993db412deb!2sHADI%20TEX%20CHEM!5e0!3m2!1sen!2sbd!4v1724729218978!5m2!1sen!2sbd"
+            width="100%"
+            height="400"
+            frameBorder="0"
+            allowFullScreen=""
+            aria-hidden="false"
+            tabIndex="0"
+            className="rounded-lg shadow-lg"
+          ></iframe>
+        </motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
