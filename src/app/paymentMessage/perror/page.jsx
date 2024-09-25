c15074ac1ba2eab74f961fdbe5093134bf5fc78d
sabbir1054 <mdsabbir1054@gmail.com>
2024-09-25 10:00:19 +0600

    bugfixed payment build update

--- src/app/paymentMessage/perror/page.jsx
+++ src/app/paymentMessage/perror/page.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import Swal from "sweetalert2";
 
 const EroorPayment = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  Swal.fire({
+    title: "Payment not completed!",
+    icon: "error",
+    timer: 1500,
+    showConfirmButton: false,
+    customClass: {
+      popup: "custom-swal-popup",
+    },
+  });
+  return <div></div>;
 };
 
 export default EroorPayment;
--- src/app/paymentMessage/psuccess/page.jsx
+++ src/app/paymentMessage/psuccess/page.jsx
@@ -1,4 +1,15 @@
+import Swal from "sweetalert2";
+
 const Success = () => {
+  Swal.fire({
+    title: "Payment Successful!",
+    icon: "success",
+    timer: 1500,
+    showConfirmButton: false,
+    customClass: {
+      popup: "custom-swal-popup",
+    },
+  });
   return <div></div>;
 };
 
