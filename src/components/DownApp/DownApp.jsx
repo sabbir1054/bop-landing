0f606073c7fc231b17499c1a57445f0cd6336fb0
sabbir1054 <mdsabbir1054@gmail.com>
2024-10-30 12:59:20 +0600

    link update

--- src/components/DownApp/DownApp.jsx
+++ src/components/DownApp/DownApp.jsx
@@ -6,8 +6,8 @@ import { ContainerScroll } from "../ui/container-scroll-animation";
 
 export function DownApp() {
   const n = useTranslations("navigation");
-    const t = useTranslations("home.downApp");
-    const locale= useLocale()
+  const t = useTranslations("home.downApp");
+  const locale = useLocale();
   return (
     <div className="downApp-container">
       <div className="flex flex-col overflow-hidden my-10">
@@ -15,7 +15,12 @@ export function DownApp() {
           titleComponent={
             <>
               <h1 className="text-4xl font-semibold text-black ">
-                <Link href={"/"}>
+                <Link
+                  target="_blank"
+                  href={
+                    "https://play.google.com/store/apps/details?id=com.bopbd.bopbd"
+                  }
+                >
                   <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-offset-2 focus:ring-offset-pink-50">
                     <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                     <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
